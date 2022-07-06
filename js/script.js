102156e40f60533a17b55e28409d71ee4af5001e
leticiapbrs <leticiapbrs@gmail.com>
2022-07-06 19:17:57 -0300

    adiciona mecanica pular e erro

--- js/script.js
+++ js/script.js
@@ -11,11 +11,25 @@ const jump = () => {
 };
 
 const loopGame = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window
+        .getComputedStyle(mario)
+        .bottom.replace("px","");
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition <80) {
     pipe.style.animation = "none";
     pipe.style.left = `${pipePosition}px`;
 
     mario.style.animation = "none";
-    mario.style.bottom = `${pipePosition}px`;
-})
+    mario.style.bottom = `${marioPosition}px`;
 
-document.addEventListener("keydown", jump);
+    mario.src = './images/mario-game-over.png';
+    mario.style.width = "75px";
+    mario.style.marginLeft = "45px";
+
+    clearInterval (loopGame)
+
+    }
+}, 10)
+
+document.addEventListener("keydown", jump)
